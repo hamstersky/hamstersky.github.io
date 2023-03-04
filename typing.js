5ac6d78236c68ddb7184faee58f3c03b6901d5bc
Dominik Chomicki <dc@twentyfour.dk>
2023-03-04 20:34:06 +0100

    change rotating text to typed.js library

---
+++ typing.js
@@ -0,0 +1,7 @@
+const options = {
+  strings: ["this is placeholder text", "yet another placeholder"],
+  typeSpeed: 75,
+  backSpeed: 50,
+  loop: true,
+};
+const typed = new Typed("#typing", options);
